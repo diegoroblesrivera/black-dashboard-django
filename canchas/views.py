a1cfdbe826ed4e9041a7aae393ec7ddc3a055ce9
DiegoR <diegoroblesrivera@gmail.com>
2025-07-25 16:06:10 -0400

    Ejemplo de form

--- canchas/views.py
+++ canchas/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import HorarioForm
 
 def test(request):
     
     # Page from the theme 
     return render(request, 'test/index.html')
 
+def crear_horario(request):
+    if request.method == 'POST':
+        form = HorarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_horarios')  # Redirecciona a donde quieras
+    else:
+        form = HorarioForm()
+    
+    return render(request, 'test/horario_form.html', {'form': form})
