bd5b7c1fe7a1dd57d1663c7242a3137f54285a4b
PabloMery~ <Pablomery2002@gmail.com>
2025-07-25 14:24:49 -0400

    Borrar Director comentado

--- canchas/models.py
+++ canchas/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-# class Director_Tecnico(models.Model):
-#     pri_nombre=models.CharField(max_length=20)
-#     sec_nombre=models.CharField(max_length=20,blank=True)
-#     pri_apellido=models.CharField(max_length=20)
-#     sec_apellido=models.CharField(max_length=20,blank=True)
-#     nacionalidad=models.CharField(max_length=30)
-#     correo_electronico=models.EmailField(max_length=30,unique=True)
-#     telefono=models.CharField(max_length=20)
-
-
-#     def __str__(self):
-#         return f"{self.pri_nombre} {self.pri_apellido}"
-    
-
-
 class Direccion(models.Model):
     nombre_calle = models.CharField(max_length=100)
     num_calle = models.IntegerField()
